709e3b5862a22e504ffa3b59e564c08b98d2422b
Stephen Chen <chendeyao000@gmail.com>
2020-02-04 02:49:34 +0800

    regular updata

--- test.cpp
+++ test.cpp
@@ -7,6 +7,19 @@ using namespace std;
 void set4(int &b) {
     b = 90;
 }
+
+bool comp(int a, int b) {
+    return (a > b);
+}
 int main() {
-    cout << (-10 < 0) << endl;
+    int c[100][2];
+    forn(i, 100) {
+        c[0][i] = i;
+    }
+    sort(c[0], c[0]+100, comp);
+    forn(i, 100) {
+        cout << c[0][i] << endl;
+    }
+    
+    cout << !12 << endl;
 }
